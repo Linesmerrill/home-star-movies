60f76c87eb44bcc34f44b44c7e249039ebe16109
Merrill Lines <linesmerrill@gmail.com>
2017-12-31 10:50:47 -0700

    index is now carousel page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var home = require('./routes/home');
+var search = require('./routes/search');
 var movies = require('./routes/movies');
 
 var app = express();
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/home', home);
+app.use('/search', search);
 app.use('/movies', movies);
 
 // catch 404 and forward to error handler
